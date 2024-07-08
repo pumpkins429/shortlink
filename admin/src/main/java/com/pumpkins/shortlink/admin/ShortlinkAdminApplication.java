6885ab760fb7b8693a18372d45806d574526c7da
curry <3187336730@qq.com>
2024-07-08 10:14:33 +0800

    pumpkins, init modules

---
+++ admin/src/main/java/com/pumpkins/shortlink/admin/ShortlinkAdminApplication.java
@@ -0,0 +1,17 @@
+package com.pumpkins.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @author      : pumpkins
+ * @date        : 2024/7/8 10:07
+ * @description : 后管启动类
+ * @Copyright   : ...
+ */
+@SpringBootApplication
+public class ShortlinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortlinkAdminApplication.class, args);
+    }
+}
