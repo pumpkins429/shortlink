2e1094cf851b32b218d2ab820f2a9fc660a90fa2
curry <3187336730@qq.com>
2024-07-17 16:05:52 +0800

    pumpkins, feat: 短链表分片

--- admin/src/test/java/com/pumpkins/shortlink/admin/test/ShardingshphereTest.java
+++ admin/src/test/java/com/pumpkins/shortlink/admin/test/ShardingshphereTest.java
@@ -9,20 +9,26 @@ package com.pumpkins.shortlink.admin.test;
 public class ShardingshphereTest {
 
     public static void main(String[] args) {
-        final String sql = "CREATE TABLE `t_user_%d` (\n" +
+        final String sql = "CREATE TABLE `t_link_%d` (\n" +
                 "                          `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-                "                          `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-                "                          `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-                "                          `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-                "                          `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-                "                          `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-                "                          `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',\n" +
+                "                          `domain` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '域名',\n" +
+                "                          `short_uri` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '短链接',\n" +
+                "                          `full_short_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '完整短链接 注意这个字段的字符集应该设为utf8bin以便区分大小写',\n" +
+                "                          `origin_url` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '原始链接',\n" +
+                "                          `click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
+                "                          `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+                "                          `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
+                "                          `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
+                "                          `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
+                "                          `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+                "                          `describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
                 "                          `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
                 "                          `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
                 "                          `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
                 "                          PRIMARY KEY (`id`),\n" +
-                "                          UNIQUE KEY `idx_unique_username` (`username`) COMMENT '唯一索引用户名'\n" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+                "                          UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;\n";
+
 
         for (int i = 0; i < 8; i++) {
             System.out.printf(sql, i);
