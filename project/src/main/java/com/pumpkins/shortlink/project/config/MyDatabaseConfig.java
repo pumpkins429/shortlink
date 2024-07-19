711ab9789fa52cf4225ac66b9521525ae067298f
curry <3187336730@qq.com>
2024-07-19 18:07:56 +0800

    pumpkins, feat: 配置分页插件

---
+++ project/src/main/java/com/pumpkins/shortlink/project/config/MyDatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.pumpkins.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * @author      : pumpkins
+ * @date        : 2024/7/19 17:57
+ * @description : ...
+ * @Copyright   : ...
+ */
+@Configuration
+public class MyDatabaseConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+
+}
