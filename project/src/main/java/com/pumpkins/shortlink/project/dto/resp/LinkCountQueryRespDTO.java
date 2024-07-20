5dd7400590c18ee6f358857e7ba68e63f178ee2e
curry <3187336730@qq.com>
2024-07-20 18:18:52 +0800

    pumpkins, fix: 补充@Data

---
+++ project/src/main/java/com/pumpkins/shortlink/project/dto/resp/LinkCountQueryRespDTO.java
@@ -0,0 +1,29 @@
+package com.pumpkins.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * @author      : pumpkins
+ * @date        : 2024/7/20 17:04
+ * @description : 查询分组下短链数量请求参数
+ * @Copyright   : ...
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkCountQueryRespDTO {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+    /**
+     * 短链数量
+     */
+    private Integer shortLinkCount;
+
+}
